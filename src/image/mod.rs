e21987856d6d7923e45d676c9355034cd0c2e1e4
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-06-01 11:53:45 -0500

    test: add template for csv_to_images func unit test

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -23,7 +23,7 @@ impl Image {
 }
 
 #[cfg(test)]
-mod image_tests {
+mod image_new_test {
     use super::*;
 
     #[test]
@@ -47,6 +47,9 @@ pub fn csv_to_images(file_path: &str) -> Result<Vec<Image>, String> {
     use std::io::{prelude::*, BufReader};
     use std::path::Path;
 
+    let IMAGE_WIDTH: usize = 28;
+    let IMAGE_HEIGHT: usize = 28;
+
     let file_path = Path::new(file_path);
     let file_path_string = file_path.display();
 
@@ -65,24 +68,50 @@ pub fn csv_to_images(file_path: &str) -> Result<Vec<Image>, String> {
     let mut images = Vec::<Image>::new();
     let mut build_matrix = Vec::<Vec<f64>>::new();
     let mut build_row = Vec::<f64>::new();
+    let mut label: usize = 0;
     let mut count: usize = 0;
+
     for line in file_buffer.lines() {
-        build_matrix.clear();
         build_row.clear();
         for value in line.unwrap().split(',') {
             let parsed_value = match value.parse::<f64>() {
-                Ok(parsed_value) => parsed_value,
+                Ok(parsed_value) => parsed_value / 256.0,
                 Err(..) => break,
             };
             build_row.push(parsed_value);
         }
         build_matrix.push(build_row.clone());
-        images.push(Image {
-            img_data: Matrix::build_from_matrix(build_matrix.clone()).unwrap(),
-            label: count,
-        });
+        if count % IMAGE_HEIGHT == 0 {
+            images.push(Image::new(
+                Matrix::build_from_matrix(build_matrix.clone()).unwrap(),
+                label,
+            ));
+            build_matrix.clear();
+            label += 1;
+        }
         count += 1;
     }
 
     Ok(images)
+}
+
+#[cfg(test)]
+mod image_csv_to_image_test {
+    use super::*;
+
+    #[test]
+    fn csv_to_images_test() {
+        let IMAGE_WIDTH: usize = 28;
+        let IMAGE_HEIGHT: usize = 28;
+
+        let mut ctrl_images = Vec::<Image>::new();
+        for label in 0..8 {
+            let mut ctrl_matrix = Matrix::new(IMAGE_HEIGHT, IMAGE_WIDTH);
+            ctrl_matrix.fill(4.32 / 256.0);
+            ctrl_images.push(Image::new(ctrl_matrix, label));
+        }
+
+        let new_images = csv_to_images("./src/image/TEST_FILE_csv_to_images.txt").unwrap();
+        assert_eq!(ctrl_images, new_images);
+    }
 }
